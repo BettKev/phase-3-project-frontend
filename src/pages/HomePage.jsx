e32a669adb7b78ad3255883ce51d42eb2c66c500
Kevin Bett <prodigy4614@gmail.com>
2024-12-18 21:34:31 +0300

    update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 function HomePage() {
   return (
+   <>
     <div className="home_component">
       <h1>Welcome to Our Homepage</h1>
       <p>
         This is the homepage of our website. Feel free to explore and learn more about what we do.
       </p>
     </div>
+    <div className="home_component2">
+
+    </div>
+   </>
   );
 }
 
